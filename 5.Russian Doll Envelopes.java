7af615baeaae19884b1db3cfa0028704ff9880d4
goelabhishek694 <goelabhishek694@gmail.com>
2021-07-20 23:51:20 +0530

    dp ques

---
+++ 5.Russian Doll Envelopes.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        // write your code here
+        Scanner scn=nw Scanner(System.in);
+        int n=scn.nextInt();
+        int[][] arr=new int[n][2];
+        for(int i=0;i<n;i++){
+            arr[i][0]=scn.nextInt();
+            arr[i][1]=scn.nextInt();
+        }
+        
+
+    }
+
+}
