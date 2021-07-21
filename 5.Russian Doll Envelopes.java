2aa317b8dab3bf91b28d109a3c1b8cdb11e5c928
goelabhishek694 <goelabhishek694@gmail.com>
2021-07-21 23:56:59 +0530

    dp ques

--- 5.Russian Doll Envelopes.java
+++ 5.Russian Doll Envelopes.java
@@ -5,7 +5,7 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
         // write your code here
-        Scanner scn=nw Scanner(System.in);
+        Scanner scn=new Scanner(System.in);
         int n=scn.nextInt();
         int[][] arr=new int[n][2];
         for(int i=0;i<n;i++){
@@ -13,7 +13,25 @@ public class Main {
             arr[i][1]=scn.nextInt();
         }
         
+        Arrays.sort(arr,(int[] a, int[] b)->{
+            if(a[0]==b[0]) return b[1]-a[1];
+            return a[0]-b[0];
+        });
+        int[]dp=new int[n];
+        Arrays.fill(dp,1);
+        int count=0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<i;j++){
+                if(arr[i][1]>arr[j][1]){
+                    if(dp[j]+1>dp[i]){
+                        dp[i]=dp[j]+1;
+                    }
+                }
+            }
+            count=Math.max(count,dp[i]);
+        }
 
+        System.out.println(count);
     }
 
 }
---
+++ 6.Min Squares.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	public static int solution(int n){
+		//write your code here
+        int root=(int)Math.floor(Math.pow(n,0.5));
+
+		return 0;
+	}public class 6.Min Squares {
+        
+    }
+    
+
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		int n = scn.nextInt();
+		System.out.println(solution(n));
+	}
+
+    
+
+	
+}
