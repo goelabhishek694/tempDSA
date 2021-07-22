2c92313163a60b5849b73ebdadf50eb0ed377f82
goelabhishek694 <goelabhishek694@gmail.com>
2021-07-22 23:53:11 +0530

    dp ques

--- 6.Min Squares.java
+++ 6.Min Squares.java
@@ -6,11 +6,22 @@ public class Main {
 	public static int solution(int n){
 		//write your code here
         int root=(int)Math.floor(Math.pow(n,0.5));
-
-		return 0;
-	}public class 6.Min Squares {
-        
-    }
+        //System.out.println(root);
+        int count=0;
+		int[] dp=new int[root];
+		for(int i=0;i<root;i++){
+			dp[i]=(i+1)*(i+1);
+		}
+		for(int i=root-1;i>=0;i--){
+			if(n-dp[i]>=0){
+			    n=n-dp[i];
+				count++;
+				i++;
+			}
+			if(n==0) return count;
+		}
+		return n;
+	}
     
 
 	public static void main(String[] args) {
