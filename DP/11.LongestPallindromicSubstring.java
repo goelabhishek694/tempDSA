bb133d7cabb0f0a7016534db9558481c0a0b931c
goelabhishek694 <goelabhishek694@gmail.com>
2021-10-17 00:10:28 +0530

    lpss

--- DP/11.LongestPallindromicSubstring.java
+++ DP/11.LongestPallindromicSubstring.java
@@ -7,18 +7,33 @@ public class Main {
         Scanner scn=new Scanner(System.in);
         String str=scn.nextLine();
 
-        lpss(str);        
+        int ans=lpss(str);        
+        System.out.println(ans);
     }
 
     public static int lpss(String str){
         int n=str.length();
-        int[][] arr=new arr[n][n];
-
+        int[][] arr=new int[n][n];
+        int longestSubString=1; 
         for(int gap=0;gap<n;gap++){
-            for(int i=0,j=gap;i<n;i++,j++){
-                if()
+            for(int i=0,j=gap;i<n && j<n;i++,j++){
+                if(gap==0){
+                    arr[i][j]=1;
+                    continue;
+                }
+                if(gap==1 && str.charAt(i)==str.charAt(j)){
+                    arr[i][j]=1;
+                    longestSubString=2;
+                }
+                else if(str.charAt(i)==str.charAt(j) && arr[i+1][j-1]==1){
+                    arr[i][j]=1;
+                    if(longestSubString<j-i+1){
+                        longestSubString=j-i+1;
+                    }
+                }
             }
         }
+        return longestSubString;
     }
 
 }
